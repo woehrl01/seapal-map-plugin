93acdf4431885165ad5e5610d0eb0dde1b69f312
Benjamin Sautermeister <apps@bsautermeister.de>
2013-04-03 15:41:38 +0200

    adjusted dependencies + comments

--- project/Build.scala
+++ project/Build.scala
@@ -8,24 +8,33 @@ object ApplicationBuild extends Build {
 
   val appName         = "maps"
   val appVersion      = "1.0-SNAPSHOT"
-  
-  lazy val s = Defaults.defaultSettings ++ Seq(jacoco.settings:_*)
 
-  val appDependencies = Seq(
-    // Add your project dependencies here,
+  // JaCoco settings  
+  lazy val jacocoSettings = Defaults.defaultSettings ++ Seq(jacoco.settings:_*)
+
+  // general project dependencies
+  val generalDependencies = Seq(
     javaCore,
     javaJdbc,
     javaEbean,
     "com.google.inject" % "guice" % "3.0",
     "com.google.inject.extensions" % "guice-multibindings" % "3.0",
-    "util" % "util_2.10" % "1.0-SNAPSHOT",
-    "boatdemo" % "boatdemo_2.10" % "1.3-SNAPSHOT" ,
-    "persondemo" % "persondemo_2.10" % "1.1-SNAPSHOT" 
+    "util" % "util_2.10" % "1.0-SNAPSHOT" 
   )
+  
+  // team component dependencies
+  val teamDependencies = Seq(
+  	// your project dependencies here...
+    "boatdemo" % "boatdemo_2.10" % "1.3-SNAPSHOT" ,
+    "persondemo" % "persondemo_2.10" % "1.1-SNAPSHOT"
+  )
+  
+  // all project dependencies
+  val appDependencies = generalDependencies ++ teamDependencies 
 
-  //val myExternalPom = super.settings ++ externalPom(baseDirectory(_ / "pom.xml"))
-  val main = play.Project(appName, appVersion, appDependencies, settings = s).settings(
+  val main = play.Project(appName, appVersion, appDependencies, settings = jacocoSettings).settings(
     
+    // disable parallel execution
     parallelExecution in jacoco.Config := false,
     
 	// known as group id...
@@ -34,10 +43,13 @@ object ApplicationBuild extends Build {
 	// disable using the Scala version in output paths and artifacts
 	crossPaths := false,
 	
+	// add additional resovers
     resolvers += "HTWG Resolver" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local",
 
+	// add publishing target
     publishTo := Some("HTWG Publish To" at "http://lenny2.in.htwg-konstanz.de:8081/artifactory/libs-snapshot-local;build.timestamp=" + new java.util.Date().getTime()), 
   
+  	// setup entry points for sonar code analyzer
   	pomExtra :=
 	  <build>
 	    <sourceDirectory>app</sourceDirectory>
