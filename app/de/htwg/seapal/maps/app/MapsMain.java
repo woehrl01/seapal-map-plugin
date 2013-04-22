c4fb122ebb1813d7ffb213f4f624f60f1921b318
Julian Mueller <jmueller@jm-designer.de>
2013-04-22 22:20:03 +0200

    added play bootstrap functionality to the textui main method. Now its possible to use the normal play conf.

--- app/de/htwg/seapal/maps/app/MapsMain.java
+++ app/de/htwg/seapal/maps/app/MapsMain.java
@@ -1,13 +1,19 @@
 package de.htwg.seapal.maps.app;
 
+import java.io.File;
 import java.util.Scanner;
+
+import play.api.Application;
+import play.api.DefaultApplication;
+import play.api.Mode;
+import play.api.Play;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
+import de.htwg.seapal.boat.app.AppMockModule;
 import de.htwg.seapal.maps.views.tui.MapsTUI;
 import de.htwg.seapal.person.app.PersonDemoMockModule;
-import de.htwg.seapal.boat.app.AppMockModule;
 
 /**
  * The maps startup class.
@@ -16,23 +22,36 @@ public class MapsMain {
 
 	/**
 	 * The programs main.
-	 * @param args The default program arguments.
+	 * 
+	 * @param args	The default program arguments.
 	 */
 	public static void main(String[] args) {
+		// Initialize Play Application to use the play environment functions...
+		Application play = new DefaultApplication(new File("."),
+				MapsMain.class.getClassLoader(), null, Mode.Dev());
+		Play.start(play);
+
 		// Set up Google Guice Dependency Injector
-		Injector injector = Guice.createInjector(new MapsMockModule(), new PersonDemoMockModule(), new AppMockModule());
-		
-		// Build up the application, resolving dependencies automatically by Guice
-		MapsTUI tui = injector.getInstance( MapsTUI.class);
+		Injector injector = Guice.createInjector(new MapsMockModule(),
+				new PersonDemoMockModule(), new AppMockModule());
+
+		// Build up the application, resolving dependencies 
+		// automatically by Guice
+		MapsTUI tui = injector.getInstance(MapsTUI.class);
 
 		tui.printTUI();
+
 		// continue to read user input on the tui until the user decides to quit
 		boolean active = true;
 		Scanner scanner = new Scanner(System.in);
+
 		while (active) {
 			active = tui.processInputLine(scanner.next());
 		}
+
 		scanner.close();
+
+		Play.stop();
 	}
 
 }
