f59febd19ae30e671cf279f716d7558b924db2c7
Lukas Woehrl <lukas@woehrl.net>
2013-04-22 21:39:05 +0200

    Improved line reading in maiImproved line reading in mainn

--- app/de/htwg/seapal/maps/app/MapsMain.java
+++ app/de/htwg/seapal/maps/app/MapsMain.java
@@ -1,38 +1,42 @@
 package de.htwg.seapal.maps.app;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import de.htwg.seapal.maps.views.tui.MapsTUI;
+import de.htwg.seapal.person.app.PersonDemoImplModule;
 import de.htwg.seapal.person.app.PersonDemoMockModule;
 import de.htwg.seapal.boat.app.AppMockModule;
 
 /**
  * The maps startup class.
- */
-public class MapsMain {
+ */ class MapsMain {
 
 	/**
 	 * The programs main.
 	 * @param args The default program arguments.
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
+	 
+	public static void main(String[] args) throws IOException {
 		// Set up Google Guice Dependency Injector
-		Injector injector = Guice.createInjector(new MapsMockModule(), new PersonDemoMockModule(), new AppMockModule());
+		Injector injector = Guice.createInjector(new MapsMockModule(), new PersonDemoImplModule(), new AppMockModule());
 		
 		// Build up the application, resolving dependencies automatically by Guice
 		MapsTUI tui = injector.getInstance( MapsTUI.class);
-
+		
 		tui.printTUI();
-		// continue to read user input on the tui until the user decides to quit
-		boolean active = true;
-		Scanner scanner = new Scanner(System.in);
-		while (active) {
-			active = tui.processInputLine(scanner.next());
-		}
-		scanner.close();
+
+		// continue to read user input on the tui until the user decides to quit	
+		InputStreamReader isr = new InputStreamReader(System.in, "UTF-8");
+		BufferedReader br = new BufferedReader(isr);
+		
+		while (tui.processInputLine(br.readLine()));
+		
 	}
 
 }
